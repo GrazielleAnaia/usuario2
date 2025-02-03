573aefe081182a15db3d99e260382d8afee6d815
Grazielle <grazielle.anaia@gmail.com>
2025-02-03 14:05:46 -0500

    Salva usuario

--- src/main/java/com/grazielleanaia/usuario2/business/converter/UsuarioConverter.java
+++ src/main/java/com/grazielleanaia/usuario2/business/converter/UsuarioConverter.java
@@ -48,6 +48,8 @@ public class UsuarioConverter {
                 .numero(telefoneDTO.getNumero())
                 .ddd(telefoneDTO.getDdd())
                 .build();
+
+
     }
 
 
--- src/main/java/com/grazielleanaia/usuario2/business/dto/TelefoneDTO.java
+++ src/main/java/com/grazielleanaia/usuario2/business/dto/TelefoneDTO.java
@@ -14,4 +14,5 @@ public class TelefoneDTO {
 
     private String ddd;
 
+
 }
