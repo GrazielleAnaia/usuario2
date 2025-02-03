f3b26526af9f7c98a1859edde6d7912696e87548
Grazielle <grazielle.anaia@gmail.com>
2025-02-03 10:40:15 -0500

    Startin project

---
+++ src/main/java/com/grazielleanaia/usuario2/Usuario2Application.java
@@ -0,0 +1,13 @@
+package com.grazielleanaia.usuario2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Usuario2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Usuario2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/grazielleanaia/usuario2/Usuario2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grazielleanaia.usuario2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Usuario2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
